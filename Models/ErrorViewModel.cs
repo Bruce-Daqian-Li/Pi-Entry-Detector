41c599ce3569ac92afe5d57250c3c6d9df9c24b9
Bruce-Daqian-Li <49639496+Bruce-Daqian-Li@users.noreply.github.com>
2019-04-15 19:47:29 +0800

    Created basic project structure

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Pi_Entry_Detector.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
